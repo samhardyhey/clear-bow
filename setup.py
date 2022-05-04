e636cb66b805f92e66da264cac0d66ff4e907229
Sam Hardy <samhardyhey@gmail.com>
2022-05-05 07:54:53 +1000

    propagate readme into pypi overview

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 REQUIREMENTS = [
     "numpy>=1.22.2",
@@ -6,11 +11,13 @@ REQUIREMENTS = [
 
 setup(
     name="clear_bow",
-    version="0.1",
+    version="0.2",
     url="https://github.com/samhardyhey/clear-bow",
     author="Sam Hardy",
     author_email="samhardyhey@gmail.com",
     packages=find_packages(),
     python_requires=">=3.6",
     install_requires=REQUIREMENTS,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
